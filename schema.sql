b0a054d0d1daf5208af46c49789a891fb69063ae
Kyle Carney <kyle.e.carney@gmail.com>
2019-03-13 18:00:45 -0400

    update schema

--- schema.sql
+++ schema.sql
@@ -7,10 +7,16 @@ CREATE TABLE schools (
   school_id INT NOT NULL AUTO_INCREMENT,
   school_name VARCHAR(100) NOT NULL,
   school_state VARCHAR(2) NOT NULL,
+  school_external_org_id INT(30) NULL
   PRIMARY KEY (school_id)
 );
 
-INSERT INTO schools (school_name, school_state)
+INSERT INTO schools (school_name, school_state, school_external_org_id)
 VALUES 
-('Villanova University', 'PA');
+('Air University', "AL", 100130743);
+('Alabama Agricultural and Mechanical University', "AL", 100130757);
+('Alabama State University', "AL", 100130759);
+('Athens State University', "AL", 100130760);
+('Auburn University', "AL", 100130761);
+('Auburn University at Montgomery', "AL", 100130784);
 SELECT * FROM schools;
