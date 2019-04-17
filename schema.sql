6c75356c514f7142a2c14f9a8b269b304aea98b3
Kyle Carney <kyle.e.carney@gmail.com>
2019-04-17 11:27:58 -0400

    update schema schools florida

--- schema.sql
+++ schema.sql
@@ -471,7 +471,7 @@ VALUES
 INSERT INTO schools (school_name, school_state, school_external_org_id)
 VALUES 
 ('Adventist University of Health Sciences', "FL", 100121322),
-/* Next Florida schools goes here */
+/* Next Florida schools goes here! */
 ('Florida Community College', "FL", ),
 ('Florida Community College', "FL", ),
 ('Florida Community College', "FL", ),
